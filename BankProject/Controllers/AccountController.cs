ee94ed7efe8a5bb9ae72163f537eec414f31025d
samtir65 <samtir65@gmail.com>
2022-11-13 01:58:41 +0330

    upload simple BankProjact

---
+++ BankProject/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankProject.Controllers;
+
+public class AccountController : Controller
+{
+    // post
+    [HttpPost]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

a0e687b05798aec7080592265f3142bbeedb158f
samtir65 <samtir65@gmail.com>
2022-11-13 23:50:35 +0330

    test

---
+++ BankProject/Controllers/VerifyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankProject.Controllers;
+
+public class VerifyController : Controller
+{
+    // Post
+    [HttpPost]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
